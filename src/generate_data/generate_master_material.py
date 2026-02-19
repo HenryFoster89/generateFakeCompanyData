3bdb057ad7b8d9d323b9e6284876fe9d47b9ce78
Enrico Stancanelli <enrico.stancanelli@gmail.com>
2026-02-19 12:27:12 +0100

    Chore: Improved generate_master material/customer

--- src/generate_data/generate_master_material.py
+++ src/generate_data/generate_master_material.py
@@ -2,7 +2,29 @@ import random
 import pandas as pd
 
 from src.utils.utils import on_going_messages
-from src.config import NUM_MATERIALS, OUTPUT_DIR
+from src.config import  OUTPUT_DIR
+
+#===============================
+# master material configuration
+#===============================
+# Number of items to create
+NUM_MATERIALS = 25
+
+PRODUCT_FAMILY = [
+        "Antibiotici", "Analgesici", "Cardiovascolari", "Antiinfiammatori",
+        "Antidiabetici", "Antipertensivi", "Antistaminici", "Vitamine"
+    ]
+
+UNITS = ["Scatole", "Flaconi", "Blister", "Confezioni"]
+
+# Unit Cost
+COST_MIN = 23.0
+COST_MAX = 42.0
+
+# Mark Up (To calculate Unit Price)
+MARKUP_MIN = 3.0
+MARKUP_MAX = 4.0
+
 
 def generate_master_material():
     """
@@ -13,28 +35,20 @@ def generate_master_material():
     """
     on_going_messages("Generating materials...")
 
-    # Categorie farmaceutiche comuni
-    categories = [
-        "Antibiotici", "Analgesici", "Cardiovascolari", "Antiinfiammatori",
-        "Antidiabetici", "Antipertensivi", "Antistaminici", "Vitamine"
-    ]
-
-    # Unità di misura
-    units = ["Scatole", "Flaconi", "Blister", "Confezioni"]
-
     materials = []
     for i in range(1, NUM_MATERIALS + 1):
         material = {
             "MaterialID": f"MAT{i:03d}",
             "MaterialName": f"Farmaco_{chr(64+i%26)}{i}",
-            "Category": random.choice(categories),
-            "UnitOfMeasure": random.choice(units),
-            "UnitCost": round(random.uniform(5, 150), 2),
-            "TargetInventoryDays": random.choice([30, 45, 60, 90])
+            "Category": random.choice(PRODUCT_FAMILY),
+            "UnitOfMeasure": random.choice(UNITS),
+            "UnitCost": round(random.uniform(COST_MIN, COST_MAX), 2),
+            #"TargetInventoryDays": random.choice([30, 45, 60, 90])
         }
         materials.append(material)
 
     df = pd.DataFrame(materials)
+    df["UnitPrice"] = round(df["UnitCost"] * random.uniform(MARKUP_MIN, MARKUP_MAX),2)
     df.to_csv(OUTPUT_DIR / "MasterMaterial.csv", index=False)
     on_going_messages("[OK] Generated MasterMaterial.csv")
     return df
