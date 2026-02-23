5ee8b1692cad43dff1294c48fc6c534ec7f71850
Enrico Stancanelli <enrico.stancanelli@gmail.com>
2026-02-23 09:35:49 +0100

    Remove build directory and update .gitignore

--- build/lib/generate_data/generate_support_value.py
+++
@@ -1,7 +0,0 @@
-import json
-
-from src.config import SEASONAL_PATTERN_PATH
-
-# Seasonal factors by month (1-12), loaded once at import time
-with open(SEASONAL_PATTERN_PATH, "r") as _f:
-    SEASONAL_FACTORS = json.load(_f)
